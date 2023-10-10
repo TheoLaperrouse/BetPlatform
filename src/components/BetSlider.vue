6f49e7d284244bb525627b6e3fe5b1f6df014af4
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-11 01:30:11 +0200

    feat: Add a slider component

---
+++ src/components/BetSlider.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-col items-center">
+        <p class="text-center">{{ sliderValue }} - {{ max - sliderValue }}</p>
+        <input type="range" min="0" :max="max" v-model="sliderValue" @input="updateSliderValue" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        max: {
+            type: Number,
+            default: 14,
+        },
+    },
+    data() {
+        return {
+            sliderValue: this.max,
+        };
+    },
+    methods: {
+        updateSliderValue(event) {
+            this.sliderValue = event.target.value;
+        },
+    },
+};
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
-        <BetSlider class="w-full"></BetSlider>
+        <BetSlider />
     </div>
 </template>
 <script>
