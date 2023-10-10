705a5c915cd2f33c52cf0594e6eb6ccbab032482
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-11 01:16:25 +0200

    feat: First commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
+        <BetSlider class="w-full"></BetSlider>
+    </div>
+</template>
+<script>
+import BetSlider from '@/components/BetSlider.vue';
+
+export default {
+    components: {
+        BetSlider,
+    },
+};
+</script>
