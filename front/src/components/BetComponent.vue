d3671caa1592fca5f32c7e3459454e882c27c962
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-15 00:01:41 +0200

    feat: Add Bet components

---
+++ front/src/components/BetComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex flex-col items-center">
+        <p class="text-center">{{ teams[0] }} : {{ sliderValue }} - {{ max - sliderValue }} : {{ teams[1] }}</p>
+        <input type="range" min="0" :max="max" v-model="sliderValue" @input="updateSliderValue" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        max: {
+            type: Number,
+            default: 14,
+        },
+        teams: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    data() {
+        return {
+            sliderValue: this.max,
+        };
+    },
+    methods: {
+        updateSliderValue(event) {
+            this.sliderValue = event.target.value;
+        },
+    },
+};
+</script>
---
+++ front/src/views/BetsPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2 class="text-2xl font-semibold mb-4">Paris à venir</h2>
+        <div class="mb-4">
+            <label for="matchDay" class="block text-gray-600 font-medium text-sm">Sélectionner la journée</label>
+            <input type="range" id="matchDay" v-model="selectedMatchDay" min="1" max="7" class="w-full mt-1" />
+        </div>
+        <div v-if="selectedMatchDay">
+            <h3 class="text-lg font-semibold mb-2">Journée {{ selectedMatchDay }}</h3>
+            <ul>
+                <li v-for="(bet, index) in upcomingBets[selectedMatchDay - 1]" :key="index">
+                    <BetComponent :teamNames="bet.teams" :betAmount="bet.amount" />
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import BetComponent from '@/components/BetComponent.vue';
+
+export default {
+    components: {
+        BetComponent,
+    },
+    props: {
+        upcomingBets: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    data() {
+        return {
+            selectedMatchDay: 1,
+        };
+    },
+};
+</script>
