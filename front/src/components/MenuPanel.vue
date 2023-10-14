2a9e6e83b8cd6fbefe6586f5b57e39b4ec7f0af4
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-15 00:02:40 +0200

    feat: Add a MenuPanel

---
+++ front/src/components/MenuPanel.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="menu">
+        <ul>
+            <li><router-link to="/">Accueil</router-link></li>
+            <li><router-link to="/paris">Paris</router-link></li>
+            <li><router-link to="/classement">Classement</router-link></li>
+            <!-- Ajoutez d'autres liens vers les pages de votre application -->
+        </ul>
+    </div>
+</template>
+
+<style>
+.menu {
+    position: fixed;
+    top: 0;
+    left: -300px; /* Réglez la largeur que vous souhaitez pour le menu dépliant */
+    width: 300px; /* Réglez la largeur que vous souhaitez pour le menu dépliant */
+    height: 100%;
+    background-color: #333;
+    color: #fff;
+    overflow-y: auto;
+    transition: left 0.3s;
+}
+
+.menu ul {
+    list-style: none;
+    padding: 0;
+}
+
+.menu li {
+    padding: 10px;
+}
+
+.menu a {
+    text-decoration: none;
+    color: #fff;
+}
+</style>
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,7 +1,15 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import LoginPage from '@/views/LoginPage.vue';
 import HomePage from '@/views/HomePage.vue';
+import BetsPage from '@/views/BetsPage.vue';
+import RankingPage from '@/views/RankingPage.vue';
 
-const routes: Array<RouteRecordRaw> = [{ path: '/', component: HomePage }];
+const routes: Array<RouteRecordRaw> = [
+    { path: '/', component: HomePage },
+    { path: '/signin', component: LoginPage },
+    { path: '/bets', component: BetsPage },
+    { path: '/ranking', component: RankingPage },
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
