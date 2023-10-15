55ede595df1385dd6a38b39c068d1f9cbe6527a8
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-10-15 14:27:41 +0200

    feat: add alert

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import eslint from 'vite-plugin-eslint';
+import Vue from '@vitejs/plugin-vue';
+import Eslint from 'vite-plugin-eslint';
 const path = require('path');
 
 export default defineConfig({
-    plugins: [vue(), eslint()],
+    plugins: [Vue(), Eslint()],
     server: {
         port: 8080,
     },
